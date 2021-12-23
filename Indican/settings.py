5b242e60bfaf5499ca9cdcf9258d2c15efb34d26
Ishaan Nagpal <ishaannagpal@Ishaans-MacBook-Air.local>
2021-12-23 23:09:23 +0530

    Generate Link

--- Indican/settings.py
+++ Indican/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-l#lu34bj85q2itwx_vfi+r-)0)!7dlh+m%p!c32ugzvn1*scia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','indican.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','indican-vraksh.herokuapp.com']
 
 
 # Application definition
